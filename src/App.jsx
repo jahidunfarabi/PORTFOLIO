7100c02cbcd9132c18d5dcbcce74e96172669de3
MD.JAHIDUN MUNTAKA FARABI <22-49201-3@student.aiub.edu>
2025-11-13 19:40:08 +0600

    new_import

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import { LoadingScreen } from "./components/LoadingScreen"
 import { Navbar } from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
+import { Home } from "./components/section/Home";
 import "./index.css";
 
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
-   const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
@@ -16,7 +18,9 @@ function App() {
         className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"
           } bg-black text-gray-100`}
       >
-   <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
 
       </div>
     </>
